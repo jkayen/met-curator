4ad0f4e293b9dca30674a485be4c3291924736ea
Jacob Kayen <jacob.kayen@gmail.com>
2018-03-23 10:36:36 -0400

    fix: removes console logs

--- src/component.js
+++ src/component.js
@@ -15,9 +15,6 @@ export class Component extends React.Component {
     })
     .then(res => res.data.getElementsByClassName('utility-menu__item--download')[0])
     .then(res => res.getElementsByTagName('a')[0].href)
-    // .then(res => res.data.getElementsByName('#collectionImage')[0])
-    // .then(res => res.getElementsByTagName('img')[0])
-    // .then(res => res.getAttributeNode('ng-src').nodeValue)
     .then(res => res.substring(res.indexOf("('") + 2, res.indexOf("')")))
     .then(res => this.setState(() => ({imageUrl: res})))
   }
